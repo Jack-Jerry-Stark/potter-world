8cac18f07e2ed5df120d9707811811907bca8a1b
Jack-Jerry-Stark <jerry.estelle.dumont@gmail.com>
2020-09-19 00:07:53 +0200

    page Characters List et Home Page ok en inté html css js

--- public/js/switchLogo.js
+++ public/js/switchLogo.js
@@ -1,69 +1,87 @@
-
 let app = {
+    // J'initialise mon app
     init: function () {
         app.addListenerToActions();
-        //app.BgMouseOver();
-        //app.BgMouseOut();
         app.setHours();
     },
-    
+
+    // Cette fonction permet d'appeler la fonction qui modifie de theme en dark ou lihgt mode
     addListenerToActions: function() {
         const chk = document.getElementById('chk');
         chk.addEventListener('change', () => {
             app.darkOrLight();
         });
     },
-    darkOrLight: function () {
-        const logo = document.getElementById('logo');
-        const body = document.getElementById('body');
-        body.classList.toggle('dark');
-        //logo.classList.remove('logo');
-        logo.classList.toggle('dark');
 
+        // Cette fonction permet de modifier le theme en dark ou lihgt mode
+    darkOrLight: function () {
+        const bg = document.getElementById('background');
+        const logo = document.getElementById('logo');
+        const soleil = document.getElementById('soleil');
+        const body = document.getElementById('body');
+        const fbackground = document.getElementById('fbackground');
+
+        body.classList.toggle('dark');
+        body.style.transition = "5s linear";
+        bg.classList.toggle('dark');
+        bg.style.transition = "5s linear";
+        logo.classList.toggle('dark');
+        logo.style.transition = "5s linear";
+        soleil.classList.toggle('darkMoon');
+        soleil.style.transition = "5s linear";
+        fbackground.classList.add('dark');
+        fbackground.style.transition = "5s linear";
     },
-    /*BgMouseOver: function() {
-    const el = document.querySelector("#bg");
-    const move = 20;
-    
-    el.addEventListener("mousemove", (e) => {
-    el.style.backgroundPositionX = -10 + "px"; 
-    el.style.backgroundPositionY = -10 + "px";
-    });
-    }BgMouseOver: function() {
-    document.querySelector(body).style.backgroundPositionY = "100px"
-    },
-    BgMouseOut: function() {
-        document.querySelector(body).style.backgroundPositionY = "0px"
-    },*/
+
+  // Fonction qui permet de changer de theme en fonction de l'heure qu'il est
     setHours: function(){
         let date = new Date();
         let hours = date.getHours();
         const body = document.getElementById('body');
+        const background = document.getElementById('background');
+        const fbackground = document.getElementById('fbackground');
+        const soleil = document.getElementById('soleil');
+        const h1 = document.getElementById('titleh1');
+        const h2 = document.getElementById('h2_title');
+        const logo = document.getElementById('logo');
+        const switchBtn = document.getElementById('btnSwitchTheme');
+
         console.log(hours);
+        
         if (hours > 20 || hours < 6 ){
+            background.classList.add('night');
+            fbackground.classList.add('night');
             body.classList.add('night');
+            soleil.classList.toggle('moon');
+            logo.classList.toggle('dark');
+            h2.style.color = '#ffffff';
+            switchBtn.style.display = 'none';
         }
         else if (hours < 9 || hours >17){
+            background.classList.add('sunlight');
+            fbackground.classList.add('sunlight');
             body.classList.add('sunlight');
+            soleil.classList.toggle('twilight');
+            h1.style.color = '#ffffff';
+            h2.style.color = '#ffffff';
+            logo.classList.toggle('dark');
         }
         else if (hours >= 9 || hours < 17){
-            body.classList.add('sun');
+            background.classList.add('sun');
+            body.classList.toggle('sun');
+            fbackground.classList.add('sun');
+            
         }
         else{
-            body.classList.add('night');
+            background.classList.add('dark');
+            body.classList.add('dark');
+            soleil.classList.toggle('darkMoon');
+            logo.classList.toggle('dark');
+            fbackground.classList.add('dark');
+            h2.style.color = '#ffffff';
+            switchBtn.style.display = 'none';
         }
-
     }
-    // BgChangeHour: function() {
-        
-
-    //     const body = document.getElementById('body');
-        
-    //     hours.addEventListener('change', () => {
-            
-    //         });
-    //     }
-        
 };
     document.addEventListener('DOMContentLoaded', app.init );
 
--- public/js/switchTheme.js
+++ public/js/switchTheme.js
@@ -1,29 +1,75 @@
-// const chk = document.getElementById('chk');
-
-// chk.addEventListener('change', () => {
-// 	document.body.classList.toggle('dark');
-// });
-
 
 let app = {
+    // J'initialise mon app
     init: function () {
         app.addListenerToActions();
+        app.setHours();
     },
-    
+    // Cette fonction permet d'appeler la fonction qui modifie de theme en dark ou lihgt mode
     addListenerToActions: function() {
         const chk = document.getElementById('chk');
         chk.addEventListener('change', () => {
             app.darkOrLight();
         });
     },
+        // Cette fonction permet de modifier le theme en dark ou lihgt mode
     darkOrLight: function () {
+        const bg = document.getElementById('background');
         const logo = document.getElementById('logo');
-        const bodyLogin = document.getElementById('bodyLogin');
-        bodyLogin.classList.toggle('dark');
-        //logo.classList.remove('logo');
+        const soleil = document.getElementById('soleil');
+        const body = document.getElementById('body');
+        const fbackground = document.getElementById('fbackground');
+
+        body.classList.toggle('dark');
+        bg.classList.toggle('dark');
         logo.classList.toggle('dark');
+        soleil.classList.toggle('moon');
+        fbackground.classList.add('dark');
+
+    },
+  // Fonction qui permet de changer de theme en fonction de l'heure qu'il est
+    setHours: function(){
+        let date = new Date();
+        let hours = date.getHours();
+        const body = document.getElementById('body');
+        const background = document.getElementById('background');
+        const fbackground = document.getElementById('fbackground');
+        const soleil = document.getElementById('soleil');
+        const h1 = document.getElementById('titleh1');
+        const logo = document.getElementById('logo');
+
+        console.log(hours);
+        if (hours > 20 || hours < 6 ){
+            background.classList.add('night');
+            fbackground.classList.add('night');
+            body.classList.add('dark');
+            soleil.classList.toggle('moon');
+            logo.classList.toggle('dark');
+
+        }
+        else if (hours < 9 || hours >17){
+            background.classList.add('sunlight');
+            fbackground.classList.add('sunlight');
+            body.classList.add('sunlight');
+            soleil.classList.toggle('twilight');
+            h1.style.color = '#ffffff';
+            logo.classList.toggle('dark');
+
+        }
+        else if (hours >= 9 || hours < 17){
+            background.classList.add('sun');
+            body.classList.toggle('sun');
+            fbackground.classList.add('sun');
+            
+        }
+        else{
+            background.classList.add('night');
+            body.classList.add('dark');
+            soleil.classList.toggle('moon');
+            logo.classList.toggle('dark');
+            fbackground.classList.add('dark');
+        }
     }
-    };
-    
+};
     document.addEventListener('DOMContentLoaded', app.init );
 
